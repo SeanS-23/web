fe2ff69be433d57bcd06cfe1d96e2edc7f7ca899
seans-23 <seanspringmann@comcast.net>
2021-06-21 18:05:36 -0400

    oml

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app dark hide-on-scroll elevation="0" color="#0A182F">
+      <v-icon size="32"> mdi-boxing-glove </v-icon>
+      <v-spacer />
+      <v-btn href="#about" text class="pl-3 pr-3 text-none">
+        <v-text class="text-none teal--text text--accent-1 pr-1"> 01. </v-text>
+        About
+      </v-btn>
+      <v-btn href="#experience" text class="pl-3 pr-3 text-none">
+        <v-text class="text-none teal--text text--accent-1 pr-1"> 02. </v-text>
+        Experience
+      </v-btn>
+      <v-btn href="#projects" text class="pl-3 pr-3 text-none">
+        <v-text class="text-none teal--text text--accent-1 pr-1"> 03. </v-text>
+        Projects
+      </v-btn>
+      <v-btn href="#contact" text class="pl-3 pr-3 text-none">
+        <v-text class="text-none teal--text text--accent-1 pr-1"> 04. </v-text>
+        Contact
+      </v-btn>
+      <v-btn color="teal accent-1" outlined class="pl-3 pr-3 text-none">
+        Resume
+      </v-btn>
+    </v-app-bar>
+
+    <login />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.login-time {
+  color: black !important;
+  background: black;
+  background-color: black !important;
+  height: 200px;
+  width: 400px;
+  position: relative;
 }
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="login">
+        <v-card-text class="login-time">
+            Login Time
+        </v-card-text>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
